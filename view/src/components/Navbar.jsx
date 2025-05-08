a2ca4f5419f38fa0051eb890cae047555023c0bc
nohan780 <fazilaahmed12@gmail.com>
2025-05-08 21:35:18 +0600

    nohan

--- view/src/components/Navbar.jsx
+++ view/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from '../../axios';
 import '../styles/Navbar.css';
-import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
     const navigate = useNavigate();
     const handleLogout = async () => {
